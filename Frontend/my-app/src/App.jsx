db74242e5c7eb7afd614fd8c28fcfc7727b9039e
SondosAlnbabteh <salehsondos135@gmail.com>
2024-09-02 11:16:26 +0300

    chef front

---
+++ Frontend/my-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+
+  return (
+    <>
+       <BrowserRouter>
+      <Routes>
+        
+   
+          {/* <Route path="/" element={<Blogs />} /> */}
+        
+       
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
